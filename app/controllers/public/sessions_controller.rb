a2416705a912d33d044620bfea095832ea3aa700
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-05-21 10:41:29 +0000

    [update]レイアウト微調整

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -32,7 +32,7 @@ class Public::SessionsController < Devise::SessionsController
     end
 
     def after_sign_in_path_for(resource)
-    gifts_path #贈り物一覧へ遷移
+      gifts_path #贈り物一覧へ遷移
     end
 
 
