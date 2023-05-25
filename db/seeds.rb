86953f1c6e3311096c6446e8adb3a4e954137425
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-05-25 03:04:03 +0000

    seedファイル完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,45 +39,52 @@ Scene.create(
 
 Gift.create(
    [
-       #TODO:　tag_nameを消す
-
-      {id: 1, customer_id: 1, scene_id: 1, name: '苺ケーキ', shop_name: 'ながのCAKE', price: 1000, tag_name: 'ケーキ,ながのCAKE', introduction: '喜んでもらえた',
+      {id: 1, customer_id: 1, scene_id: 1, name: '苺ケーキ', shop_name: 'ながのCAKE', price: 1000, introduction: '喜んでもらえた',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/itigocake.jpg"))},
-      {id: 2, customer_id: 1, scene_id: 2, name: '日本酒', shop_name: '定〇渓商店', price: 2000, tag_name: 'お酒', introduction: 'とても良かった',
+      {id: 2, customer_id: 1, scene_id: 2, name: '日本酒', shop_name: '定〇渓商店', price: 2000, introduction: 'とても良かった',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/nihonsyu.jpg"))},
-      {id: 3, customer_id: 2, scene_id: 3, name: 'お出汁', shop_name: '久〇福商店', price: 1500, tag_name: '出汁', introduction: '喜んでもらえた',
+      {id: 3, customer_id: 2, scene_id: 3, name: 'お出汁', shop_name: '久〇福商店', price: 1500, introduction: '喜んでもらえた',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/dasi.jpg"))},
-      {id: 4, customer_id: 2, scene_id: 4, name: 'ディフューザー', shop_name: 'ギフトショップ', price: 1300, tag_name: 'ディフューザー', introduction: 'とてもよかった',
+      {id: 4, customer_id: 2, scene_id: 4, name: 'ディフューザー', shop_name: 'ギフトショップ', price: 1300, introduction: 'とてもよかった',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/diffuser.jpg"))},
-      {id: 5, customer_id: 3, scene_id: 5, name: '香水', shop_name: 'Shir〇', price: 4000, tag_name: '香水', introduction: '喜んでもらえた',
+      {id: 5, customer_id: 3, scene_id: 5, name: '香水', shop_name: 'Shir〇', price: 4000, introduction: '喜んでもらえた',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/kousui.jpg"))},
-      {id: 6, customer_id: 3, scene_id: 6, name: '石鹸', shop_name: 'L〇SH', price: 700, tag_name: '石鹸', introduction: '良かった',
+      {id: 6, customer_id: 3, scene_id: 6, name: '石鹸', shop_name: 'L〇SH', price: 700, introduction: '良かった',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/sekkenn.jeg"))},
-      {id: 7, customer_id: 4, scene_id: 1, name: 'ドライフラワー', shop_name: 'フラワーショップ', price: 1300, tag_name: '花', introduction: 'とても良かった',
+      {id: 7, customer_id: 4, scene_id: 1, name: 'ドライフラワー', shop_name: 'フラワーショップ', price: 1300, introduction: 'とても良かった',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/hana.jpg"))},
-      {id: 8, customer_id: 4, scene_id: 2, name: 'ココアケーキ', shop_name: 'ながのCAKE', price: 700, tag_name: 'ケーキ,ながのCAKE', introduction: '喜んでもらえた',
+      {id: 8, customer_id: 4, scene_id: 2, name: 'ココアケーキ', shop_name: 'ながのCAKE', price: 700, introduction: '喜んでもらえた',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/cocoacake.jpg"))},
-      {id: 9, customer_id: 5, scene_id: 3, name: 'チョコレート', shop_name: '〇〇製菓', price: 1300, tag_name: 'チョコレート', introduction: '喜んでもらえた',
+      {id: 9, customer_id: 5, scene_id: 3, name: 'チョコレート', shop_name: '石〇製菓', price: 1300, introduction: '喜んでもらえた',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/choco.jpg"))},
-      {id: 10, customer_id: 5, scene_id: 4, name: 'プリン', shop_name: 'ながのCAKE', price: 300, tag_name: 'プリン,ながのCAKE', introduction: '喜んでもらえた',
+      {id: 10, customer_id: 5, scene_id: 4, name: 'プリン', shop_name: 'ながのCAKE', price: 300, introduction: '喜んでもらえた',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/purin.jpg"))},
-      {id: 11, customer_id: 5, scene_id: 5, name: 'クッキー', shop_name: 'ながのCAKE', price: 300, tag_name: 'クッキー,ながのCAKE', introduction: '喜んでもらえた',
+      {id: 11, customer_id: 5, scene_id: 5, name: 'クッキー', shop_name: 'ながのCAKE', price: 300, introduction: '喜んでもらえた',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/cookie.jpg"))},
-      {id: 12, customer_id: 5, scene_id: 6, name: 'ハーバリウム', shop_name: 'ギフトショップ', price: 700, tag_name: '花', introduction: '良かった',
+      {id: 12, customer_id: 5, scene_id: 6, name: 'ハーバリウム', shop_name: 'ギフトショップ', price: 700, introduction: '良かった',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/herbarium.jpg"))},
-      {id: 13, customer_id: 5, scene_id: 1, name: 'カボチャムース', shop_name: '〇〇製菓', price: 300, tag_name: 'かぼちゃ', introduction: '良かった',
+      {id: 13, customer_id: 5, scene_id: 1, name: 'カボチャムース', shop_name: '石〇製菓', price: 300, introduction: '良かった',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/kabotya.jpg"))},
-      {id: 14, customer_id: 5, scene_id: 1, name: 'ミックスナッツ', shop_name: '〇〇製菓', price: 300, tag_name: 'ナッツ', introduction: '喜んでもらえた',
+      {id: 14, customer_id: 5, scene_id: 1, name: 'ミックスナッツ', shop_name: '石〇製菓', price: 300, introduction: '喜んでもらえた',
                profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/gifts/nattu.jpg"))},
    ]
 )
 
-## TODO: Tagのseedをつくる
+
 Tag.create!(
    [
       {id: 1, name: 'ケーキ'},
       {id: 2, name: 'ながのCAKE'},
       {id: 3, name: 'お酒'},
+      {id: 4, name: '出汁'},
+      {id: 5, name: 'ディフューザー'},
+      {id: 6, name: '香水'},
+      {id: 7, name: '石鹸'},
+      {id: 8, name: '花'},
+      {id: 9, name: 'チョコレート'},
+      {id: 10, name: 'プリン'},
+      {id: 11, name: 'クッキー'},
+      {id: 12, name: 'ナッツ'},
    ]
 )
 
@@ -86,6 +93,21 @@ GiftTag.create!(
       {id: 1, tag_id: 1, gift_id: 1},
       {id: 2, tag_id: 2, gift_id: 1},
       {id: 3, tag_id: 3, gift_id: 2},
+      {id: 4, tag_id: 4, gift_id: 3},
+      {id: 5, tag_id: 5, gift_id: 4},
+      {id: 6, tag_id: 6, gift_id: 5},
+      {id: 7, tag_id: 7, gift_id: 6},
+      {id: 8, tag_id: 8, gift_id: 7},
+      {id: 9, tag_id: 1, gift_id: 8},
+      {id: 10, tag_id: 2, gift_id: 8},
+      {id: 11, tag_id: 9, gift_id: 9},
+      {id: 12, tag_id: 2, gift_id: 10},
+      {id: 12, tag_id: 10, gift_id: 10},
+      {id: 13, tag_id: 2, gift_id: 11},
+      {id: 13, tag_id: 11, gift_id: 11},
+      {id: 14, tag_id: 8, gift_id: 12},
+      {id: 15, tag_id: 1, gift_id: 13},
+      {id: 16, tag_id: 12, gift_id: 14},
 
    ]
 )
